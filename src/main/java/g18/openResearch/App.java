130b3afdfcc75320abcce756558ab1de4023ed06
cagatay iba <cagatay.iba@gmail.com>
2023-05-17 19:22:48 +0300

    initial release

---
+++ src/main/java/g18/openResearch/App.java
@@ -0,0 +1,13 @@
+package g18.openResearch;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
